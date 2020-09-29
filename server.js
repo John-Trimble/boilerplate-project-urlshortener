07588899726758b187581ee84d66f6ab4acd3661
John Henry Trimble <JHT@MacBook-Pro-5.local>
2020-09-29 15:08:06 -0700

    Update server.js

--- server.js
+++ server.js
@@ -1,11 +1,9 @@
 'use strict';
 var dns = require('dns')
 var express = require('express');
-var mongo = require('mongodb');
 var mongoose = require('mongoose');
 var bodyParser = require('body-parser')
 var cors = require('cors');
-const { stringify } = require('querystring');
 require('dotenv')
 
 var app = express();
@@ -14,7 +12,8 @@ var port = process.env.PORT || 3000;
 
 process.env.MONGO_URI="mongodb+srv://John:3216@fcc-cluster.edllc.mongodb.net/URLShortener?retryWrites=true&w=majority"
 
-mongoose.connect(process.env.MONGO_URI, {useNewUrlParser: true, useFindAndModify: true, useUnifiedTopology: true})
+var connection = mongoose.connect(process.env.MONGO_URI, {useNewUrlParser: true, useFindAndModify: true, useUnifiedTopology: true})
+
 
 var db = mongoose.connection;
 db.on("error", console.error.bind(console, "Conection error"))
@@ -22,8 +21,10 @@ db.once("open", console.log.bind(console, "Connected"))
 
 const ShortsSchema = mongoose.Schema({
   origin: {type: String, required: true},
-  new: {type: Number, required: true, unique: true}
+  new: {type: Number, required: true, unique: true, default: 0}
 })
+
+const Shorts = mongoose.model("Shorts", ShortsSchema)
 
 app.use(cors());
 
@@ -36,11 +37,30 @@ app.get('/', function(req, res){
 });
   
 app.post("/api/shorturl/new", (req, res) => {
-  res.send({url: req.body.url})
+  const url = req.body.url.split("/").slice(2,).join("/")
+  dns.lookup(url, (err, adresses, family) => {
+    if (err) res.send({error: "Invalid URL"})
+    else {
+      Shorts.estimatedDocumentCount((err, count) => {
+        if (err) res.send({error: "Failed to fetch number of documents from the database"});
+        var short = new Shorts({origin: req.body.url, new: count + 1})
+        short.save((err, data) => {
+          if (err) res.send({error: "Failed to save URL"})
+          else res.send({url: data.origin, new: data.new})
+        })
+      })
+    }
+  })
 })
 
 app.get("/api/shorturl/:index", (req, res) => {
-  res.redirect("/")
+  if (!+req.params.index) res.redirect("/")
+  else {
+    Shorts.findOne({new: req.params.index}, (err, data) => {
+      err ? res.send({error: "invalid URL"})
+      : res.redirect(data.origin)
+    })
+  }
 })
 
 app.listen(port, function () {
