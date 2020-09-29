71073da7c424d3e3a17f316a1657964e2193d62f
John Henry Trimble <JHT@macbook-pro-5.cate.org>
2020-09-28 21:13:54 -0700

    Update server.js

--- server.js
+++ server.js
@@ -1,36 +1,47 @@
 'use strict';
-
+var dns = require('dns')
 var express = require('express');
 var mongo = require('mongodb');
 var mongoose = require('mongoose');
-
+var bodyParser = require('body-parser')
 var cors = require('cors');
+const { stringify } = require('querystring');
+require('dotenv')
 
 var app = express();
 
-// Basic Configuration 
 var port = process.env.PORT || 3000;
 
-/** this project needs a db !! **/ 
-// mongoose.connect(process.env.DB_URI);
+process.env.MONGO_URI="mongodb+srv://John:3216@fcc-cluster.edllc.mongodb.net/URLShortener?retryWrites=true&w=majority"
+
+mongoose.connect(process.env.MONGO_URI, {useNewUrlParser: true, useFindAndModify: true, useUnifiedTopology: true})
+
+var db = mongoose.connection;
+db.on("error", console.error.bind(console, "Conection error"))
+db.once("open", console.log.bind(console, "Connected"))
+
+const ShortsSchema = mongoose.Schema({
+  origin: {type: String, required: true},
+  new: {type: Number, required: true, unique: true}
+})
 
 app.use(cors());
 
-/** this project needs to parse POST bodies **/
-// you should mount the body-parser here
+app.use(bodyParser.urlencoded({extended: false}))
 
 app.use('/public', express.static(process.cwd() + '/public'));
 
 app.get('/', function(req, res){
   res.sendFile(process.cwd() + '/views/index.html');
 });
-
   
-// your first API endpoint... 
-app.get("/api/hello", function (req, res) {
-  res.json({greeting: 'hello API'});
-});
+app.post("/api/shorturl/new", (req, res) => {
+  res.send({url: req.body.url})
+})
 
+app.get("/api/shorturl/:index", (req, res) => {
+  res.redirect("/")
+})
 
 app.listen(port, function () {
   console.log('Node.js listening ...');
